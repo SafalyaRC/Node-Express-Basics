f00854c39f50c1c76ed9e18fbc517dc42b87d8d8
SafalyaRC <safalyaroy9463@gmail.com>
2025-07-24 20:25:17 +0530

    path module in NodeJS

---
+++ 01-intro-js/pathModule.js
@@ -0,0 +1,6 @@
+const path=require('path')
+
+console.log(path.sep);
+
+// path.resolve(_dirname,'folder','filename') --> returns the absolute path of the file specified 
+// path.join('folder','file') --> joins the paths
